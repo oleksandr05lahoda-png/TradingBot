9e54b1adbbda6769dea4630dc4b9a9050c03b44d
Asus_F15 <oleksandr05lahoda@gmail.com>
2025-11-22 20:45:29 +0100

    Initial commit

--- src/main/java/com/bot/BotMain.java
+++ src/main/java/com/bot/BotMain.java
@@ -2,8 +2,9 @@ package com.bot;
 
 public class BotMain {
     public static void main(String[] args) {
+        System.setProperty("java.awt.headless", "true");
         System.out.println("Trading Telegram Bot запущен...");
         SignalSender sender = new SignalSender();
-        sender.start(); // <-- запускаем новый метод
+        sender.start();
     }
 }
