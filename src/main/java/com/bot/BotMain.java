010a5e1ea072c0d297a29aaac8da48671102bdae
Asus_F15 <oleksandr05lahoda@gmail.com>
2025-11-22 18:43:05 +0100

    Fix BotMain to use SignalSender.start()

--- src/main/java/com/bot/BotMain.java
+++ src/main/java/com/bot/BotMain.java
@@ -4,6 +4,6 @@ public class BotMain {
     public static void main(String[] args) {
         System.out.println("Trading Telegram Bot запущен...");
         SignalSender sender = new SignalSender();
-        sender.runScheduler();
+        sender.start(); // <-- запускаем новый метод
     }
 }
